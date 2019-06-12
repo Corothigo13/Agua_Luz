05bf7a72af5d64ba49d07a4be234a00ee20182da
Marcos Almeida <monitoria.puccontagem@gmail.com>
2019-06-12 18:57:42 -0300

    first commit

---
+++ IAgua.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AguaLuz1
+{
+    interface IAgua
+    {
+        double CalcularConsumo();
+        void CalcularConta();
+    }
+    //iagua
+}
